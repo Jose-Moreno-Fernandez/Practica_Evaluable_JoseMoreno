78e69a86271b9d9e6e56d8aad3b0c919a78cb7db
Jose-Moreno-Fernandez <jose.mfe2006@gmail.com>
2025-02-07 13:59:09 +0100

    Documentación en rama getter

--- src/Computer.java
+++ src/Computer.java
@@ -1,6 +1,6 @@
 /**
  * Clase computer que represanta a ordenadores en general
- * @author Manuel Molino && Encarni Muñoz
+ * @author Manuel Molino && Encarni Muñoz && Jose Moreno
  * @version 1.0.0
  */
 public class Computer {
@@ -8,5 +8,26 @@ public class Computer {
     private float cpu;
     private String motherBoard;
 
-    
+    /**
+     * Getter que nos devuelve el valor int de la ram
+     * @return La cantidad de ram del ordenador
+     */
+    public int getRam() {
+        return ram;
+    }
+    /**
+     * Getter que nos devuelve el valor float de la cpu
+     * @return La cpu del ordenador
+     */
+    public float getCpu() {
+        return cpu;
+    }
+
+    /**
+     * Getter que nos devuelve el valor String de la placa base
+     * @return La placa base del ordenador
+     */
+    public String getMotherBoard() {
+        return motherBoard;
+    }
 }
